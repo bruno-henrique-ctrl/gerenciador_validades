379cddab0f77455adb39e5bce9a1f21f7d95d9e6
bruno-henrique-crtl <bruno.henriqueds@outlook.com.br>
2025-11-27 21:55:01 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Gerenciador de Validades",
   description: "Gerenciador de Validades para produtos do mercado",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
